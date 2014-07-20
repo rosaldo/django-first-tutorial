96b0cfd4f92465316ebc93660e8385b81accaa88
rosaldo <rosaldo@gmail.com>
2014-07-20 13:27:31 -0300

    Insercao do objeto Choice na interface administrativa

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Poll, Choice
 
 
 class PollAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ class PollAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
