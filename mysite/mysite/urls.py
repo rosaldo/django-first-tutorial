8c525ecc4a93491eb37eb50821a10957851c66ae
rosaldo <rosaldo@gmail.com>
2014-07-20 17:52:56 -0300

    Trabalhando com o paramentro namespace para o include da url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,8 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'mysite.views.home', name='home'),
-                       url(r'^polls/', include('polls.urls')),
+                       url(r'^polls/', include('polls.urls',
+                                               namespace='polls')),
                        url(r'^admin/', include(admin.site.urls)),
                        )
