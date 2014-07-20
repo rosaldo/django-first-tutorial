e07d056f5f1b42695f8d5b2ca50b803d82068259
rosaldo <rosaldo@gmail.com>
2014-07-20 13:15:42 -0300

    Personalizando a ordem de exibicao dos campos do objeto poll

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from polls.models import Poll
 
-admin.site.register(Poll)
+
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+
+admin.site.register(Poll, PollAdmin)
