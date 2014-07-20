2af84bd90cdf194d09377d8500fa487dac4d98e1
rosaldo <rosaldo@gmail.com>
2014-07-20 15:47:37 -0300

    Criando a primeira view do app Polls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,7 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'mysite.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
-
+                       url(r'^polls/', include('polls.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        )
