01e83f39032c1f209f27a44f0f6bebf3ce6d2b7d
rosaldo <rosaldo@gmail.com>
2014-07-19 23:37:24 -0300

    normalizacao pep8

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'mysite.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
