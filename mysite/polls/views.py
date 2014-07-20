5256266af65cc2b9edaff51cf66d80804a235fbd
rosaldo <rosaldo@gmail.com>
2014-07-20 17:07:58 -0300

    Usando o atalho render()

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,12 @@
 from django.http import HttpResponse
-from django.template import RequestContext, loader
-
+from django.shortcuts import render
 from polls.models import Poll
 
 
 def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, poll_id):
